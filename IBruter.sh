6826df23e0c1bbfcb7bae4b65f55c947dd6452c3
Harsh Malhotra <71483241+harsh2527@users.noreply.github.com>
2020-10-02 00:56:47 +0530

    Update IBruter.sh

--- IBruter.sh
+++ IBruter.sh
@@ -16,7 +16,7 @@ var2=$(echo $var | awk -F ';' '{print $2}' | cut -d '=' -f3)
 
 checkroot() {
 if [[ "$(id -u)" -ne 0 ]]; then
-    printf "\e[1;77mPlease, run this program as root!\n\e[0m"
+    printf "\e[1;77mPlease run me as root!\n\e[0m"
     exit 1
 fi
 }
